c3da5bddccc0263ff3b37e524526420d91dd1087
Tim <156736489+tim11451@users.noreply.github.com>
2025-11-20 22:04:01 +0800

    Add twoSum function to Solution class
    Implemented a brute-force solution for the two-sum problem.

--- Question1.py
+++ Question1.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        solution = [-1,-1]
+        len1 = len(nums)
+        for i in range(len1):
+            for j in range(i+1,len1):
+                if (nums[i]+nums[j]==target):
+                    return [i,j]
+        return solution                
 
+# Loser's solution. Pure brute force, just letting you memorize the experience that being slapped by primary school teacher.
+# Unfortunately, weak.
